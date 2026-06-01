9ad877e357435794919da44e998a6ca33c21d30e
WaveAudit Auto <nwbuildpro@gmail.com>
2026-06-01 00:51:35 -0700

    Revert "feat(clients): add internal client directory"
    This reverts commit b73c840c0a953ad639f114acb3e85a99c1c270ae.

--- src/app/(manager)/clients/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ClientsPage } from "@/components/manager/ClientsPage";
-import { getClientsDirectoryData } from "@/lib/clients-data";
-
-export const revalidate = 0;
-
-export default async function ClientsRoutePage() {
-  const data = await getClientsDirectoryData();
-  return <ClientsPage clients={data.clients} />;
-}
